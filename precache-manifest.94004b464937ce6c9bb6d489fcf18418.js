1e5dc8bef82f0cbdb922a3d6cd7b2eb128907549
Carrji22 <carrjim22@gmail.com>
2018-11-29 16:54:10 -0600

    Mi proyecto en React consumiendo una api de news api

---
+++ precache-manifest.94004b464937ce6c9bb6d489fcf18418.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "90b7898ef1d052712680",
+    "url": "/static/js/main.90b7898e.chunk.js"
+  },
+  {
+    "revision": "dd3209505c0cd29018ca",
+    "url": "/static/js/1.dd320950.chunk.js"
+  },
+  {
+    "revision": "90b7898ef1d052712680",
+    "url": "/static/css/main.c9e32c7f.chunk.css"
+  },
+  {
+    "revision": "299b383eff3d4e54b0b3aff9b943e25f",
+    "url": "/index.html"
+  }
+];
